3fae50726f8869d25f2691c70513a18124c8d98d
eee11223344 <164818913+eee11223344@users.noreply.github.com>
2024-11-13 17:11:49 +0800

    Update create.js

--- create.js
+++ create.js
@@ -1,21 +1,20 @@
-/* Jason SunYingjie */
+document.addEventListener('DOMContentLoaded', function() {
+  const registerForm = document.getElementById('registerForm');
 
+  registerForm.addEventListener('submit', function(event) {
+      event.preventDefault(); // Prevent form submission
 
+      let username = document.getElementById('username').value;
+      let password = document.getElementById('password').value;
 
-document.getElementById('registerForm').addEventListener('submit', function(event) {  
-    event.preventDefault(); // Prevent form submission  
-  
-    let username = document.getElementById('username').value;  
-    let password = document.getElementById('password').value;  
-  
-    localStorage.setItem("username", username);
-    localStorage.setItem("password", password);
-  
-    console.log("Stored user name:" + localStorage.getItem("username"));
-    console.log("Stored password:" + localStorage.getItem("password"));
-    
-    alert('Registration successful! Please log in.');
-    window.location.href = 'login.html'; // Redirect to login page after registration
+      localStorage.setItem("username", username);
+      localStorage.setItem("password", password);
+
+      console.log("Stored user name:" + localStorage.getItem("username"));
+      console.log("Stored password:" + localStorage.getItem("password"));
+
+      alert('Registration successful! Please log in.');
+      localStorage.setItem('accountCreated', 'true'); // Set the sign that the account has been created
+      window.location.href = 'login.html'; // Redirect to login page
   });
-  
-  
+});
